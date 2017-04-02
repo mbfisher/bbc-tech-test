ac7f09b123eb1adb8476dd907a6729553e86cdd9
Mike Fisher <mike@mbfisher.com>
2017-04-02 17:23:39 +0100

    1-10!

--- RomanNumerals.js
+++ RomanNumerals.js
@@ -1,5 +1,20 @@
+const NUMERALS = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+};
+
 function generate(integer) {
-    return 'I';
+  if (integer <= 10) {
+    return NUMERALS[integer];
+  }
 }
 
 module.exports = { generate };
