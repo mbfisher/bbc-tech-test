8c112759918ec699ed5d7cc7719ed65e70c5bae1
Mike Fisher <mike@mbfisher.com>
2017-04-02 18:21:15 +0100

    Add test for upper and lower bounds

--- RomanNumerals.test.js
+++ RomanNumerals.test.js
@@ -10,4 +10,9 @@ describe('RomanNumerals', () => {
       expect(actual).to.equal(expected, `Expected ${i} to convert to ${expected}`);
     }
   });
+
+  it('throws if any other integers are passed', () => {
+    expect(() => generate(0)).to.throw(/between 1 and 3999/);
+    expect(() => generate(4000)).to.throw(/between 1 and 3999/);
+  });
 });
